a7dbe7d8ec2888aaca1f22efead7449405423bd7
Malika Uteuliyeva <37931254+uteuliyeva@users.noreply.github.com>
2022-02-23 15:27:23 +0600

    Initial solution for 704. Binary Search Adding comments on date/difficulty to 35.

--- log/704_binary_search.py
+++ log/704_binary_search.py
@@ -1,7 +1,7 @@
 #Date: 022322
 #Difficulty: Easy
 class Solution(object):
-    def searchInsert(self, nums, target):
+    def search(self, nums, target):
         """
         :type nums: List[int]
         :type target: int
@@ -17,4 +17,4 @@ class Solution(object):
                 left=mid+1
             else:
                 right=mid-1
-        return left
+        return -1
