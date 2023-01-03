51bd9ec3ea27aad8a5b5f2a48ac2953f26073cd4
Malika Uteuliyeva <37931254+uteuliyeva@users.noreply.github.com>
2023-01-03 17:54:44 +0100

    Initial solution to 944.

---
+++ log/944_Delete_Columns_to_Make_Sorted.py
@@ -0,0 +1,20 @@
+#Date: 030123
+#Difficulty: Easy
+class Solution(object):
+    def minDeletionSize(self, strs):
+        """
+        :type strs: List[str]
+        :rtype: int
+        """
+        N = len(strs)
+        L = len(strs[0])
+        res = 0
+        for c in range(L):
+            curr=strs[0][c]
+            for r in range(N):
+                if ord(strs[r][c])<ord(curr):
+                    res+=1
+                    break
+                else:
+                    curr=strs[r][c]
+        return res
