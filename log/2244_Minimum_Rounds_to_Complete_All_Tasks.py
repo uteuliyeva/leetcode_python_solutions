8bf62923c60eb887141cea89a1d5a9cac2138bf4
Malika Uteuliyeva <37931254+uteuliyeva@users.noreply.github.com>
2023-01-04 13:17:38 +0100

    Initial solution to 2244.

---
+++ log/2244_Minimum_Rounds_to_Complete_All_Tasks.py
@@ -0,0 +1,16 @@
+#Date: 040222
+#Difficulty: Medium
+class Solution(object):
+    def minimumRounds(self, tasks):
+        """
+        :type tasks: List[int]
+        :rtype: int
+        """
+        count=Counter(tasks)
+        res=0
+        for i in count.keys():
+            if count[i]==1:
+                return -1
+            res+=(count[i]//3)
+            res+=1 if count[i]%3>0 else 0
+        return res
