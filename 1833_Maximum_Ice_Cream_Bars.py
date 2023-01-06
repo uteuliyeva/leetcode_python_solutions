4dba58ef5e8c218ff64011e81f29574ca4a59c59
Malika Uteuliyeva <37931254+uteuliyeva@users.noreply.github.com>
2023-01-06 08:14:04 +0100

    Initial solution to 1833.

---
+++ 1833_Maximum_Ice_Cream_Bars.py
@@ -0,0 +1,17 @@
+#Date: 060123
+#Difficulty: Medium
+class Solution(object):
+    def maxIceCream(self, costs, coins):
+        """
+        :type costs: List[int]
+        :type coins: int
+        :rtype: int
+        """
+        costs.sort()
+        res=0
+        for i in range(len(costs)):
+            if costs[i]>coins:
+                return res
+            res+=1
+            coins-=costs[i]
+        return res
